a90a0a43f5c1d4dd1feaaf8f0dc21bbdb9eae06a
PY44N <pyan4444@gmail.com>
2022-05-10 20:58:18 -0400

    Started on the vector 2 class

--- src/main/kotlin/cshcyberhawks/swolib/math/Vector2.kt
+++ src/main/kotlin/cshcyberhawks/swolib/math/Vector2.kt
@@ -1,5 +1,20 @@
 package cshcyberhawks.swolib.math
 
 class Vector2 {
+    var x: Double = 0.0
+    var y: Double = 0.0
 
+    constructor(x: Number, y: Number) {
+        this.x = x.toDouble()
+        this.y = y.toDouble()
+    }
+
+    operator fun plusAssign(other: Vector2) {
+        this.x += other.x
+        this.y += other.y
+    }
+
+    operator fun plus(other: Vector2): Vector2 {
+        return Vector2(this.x + other.x, this.y + other.y)
+    }
 }
