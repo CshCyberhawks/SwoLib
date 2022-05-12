1dc462bcb113f8fce28a1d3320e53a975da212c2
PY44N <pyan4444@gmail.com>
2022-05-11 21:06:41 -0400

    Documented swo conversions

--- src/main/kotlin/cshcyberhawks/swolib/math/SwoConversions.kt
+++ src/main/kotlin/cshcyberhawks/swolib/math/SwoConversions.kt
@@ -1,10 +1,23 @@
 package cshcyberhawks.swolib.math
 
+/**
+ * A collection of methods for calculations related to our invented unit the "swo".
+ */
 object SwoConversions {
+    /**
+     * Converts from swos to meters.
+     *
+     * @param swos The number of swos traveled.
+     */
     fun swosToMeters(swos: Number): Number {
         return swos.toDouble() * 0.27083333333
     }
 
+    /**
+     * Converts from meters to swos.
+     *
+     * @param meters The number of meters traveled.
+     */
     fun metersToSwos(meters: Number): Number {
         return meters.toDouble() * 3.69230769231
     }
