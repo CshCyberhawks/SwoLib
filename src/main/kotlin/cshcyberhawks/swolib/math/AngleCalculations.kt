404d4fa957845bf24a40af0b2fc8f60371047b30
PY44N <pyan4444@gmail.com>
2022-05-11 20:53:47 -0400

    Documented angle calculations

--- src/main/kotlin/cshcyberhawks/swolib/math/AngleCalculations.kt
+++ src/main/kotlin/cshcyberhawks/swolib/math/AngleCalculations.kt
@@ -2,7 +2,13 @@ package cshcyberhawks.swolib.math
 
 import kotlin.math.abs
 
+/**
+ * A collection of methods for calculations related to angles.
+ */
 object AngleCalculations {
+    /**
+     * A function that changes an angle range from -180-180 to 0-360.
+     */
     fun wrapAroundAngles(angle: Number): Number {
         var angle: Double = angle.toDouble()
         while (angle < 0) {
@@ -11,6 +17,9 @@ object AngleCalculations {
         return angle
     }
 
+    /**
+     * A function that calculates the which way a wheel should be facing for the least rotation.
+     */
     fun optimizeAngle(desiredAngle: Number, currentAngle: Number): Number {
         val desiredAngle: Double = desiredAngle.toDouble()
         val currentAngle: Double = currentAngle.toDouble()
