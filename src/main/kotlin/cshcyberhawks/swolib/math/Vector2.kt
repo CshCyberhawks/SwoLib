2c8b737cd25f1f809cca6f4c6842fcee3dfc4613
PY44N <pyan4444@gmail.com>
2022-05-11 20:46:02 -0400

    Wrote docs for Vector2

--- src/main/kotlin/cshcyberhawks/swolib/math/Vector2.kt
+++ src/main/kotlin/cshcyberhawks/swolib/math/Vector2.kt
@@ -1,27 +1,52 @@
 package cshcyberhawks.swolib.math
 
+/**
+ * A class for managing a 2d vector (coordinate).
+ *
+ * A class for managing a 2d vector (coordinate).
+ *
+ * @property x X value for the coordinate.
+ * @property y Y value for the coordinate.
+ *
+ * @constructor Creates a vector2 with the specified x and y value.
+ */
 class Vector2(x: Number = 0, y: Number = 0) {
     var x: Double = x.toDouble()
     var y: Double = y.toDouble()
 
+    /**
+     * Override for the += operator.
+     */
     operator fun plusAssign(other: Vector2) {
         this.x += other.x
         this.y += other.y
     }
 
+    /**
+     * Override for the + operator.
+     */
     operator fun plus(other: Vector2): Vector2 {
         return Vector2(this.x + other.x, this.y + other.y)
     }
 
+    /**
+     * Override for the -= operator.
+     */
     operator fun minusAssign(other: Vector2) {
         this.x -= other.x
         this.y -= other.y
     }
 
+    /**
+     * Override for the - operator.
+     */
     operator fun minus(other: Vector2): Vector2 {
         return Vector2(this.x - other.x, this.y - other.y)
     }
 
+    /**
+     * Override for the == and != operators.
+     */
     override operator fun equals(other: Any?): Boolean {
         if (other !is Vector2) return false
         return this.x == other.x && this.y == other.y
