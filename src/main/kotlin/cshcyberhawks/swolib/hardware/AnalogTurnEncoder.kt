6de7f90352f2cea109ce63d37240efc64101e976
PY44N <pyan4444@gmail.com>
2022-05-13 11:24:26 -0400

    Added AnalogTurnEncoder

---
+++ src/main/kotlin/cshcyberhawks/swolib/hardware/AnalogTurnEncoder.kt
@@ -0,0 +1,25 @@
+package cshcyberhawks.swolib.hardware
+
+import edu.wpi.first.wpilibj.AnalogInput
+
+/**
+ * A helper class for using analog encoders for turning motors in swerve drive.
+ *
+ * @constructor Creates the encoder.
+ */
+class AnalogTurnEncoder(private val port: Int) {
+    val encoder: AnalogInput = AnalogInput(port)
+
+    private fun voltageToDegrees(input: Double): Double {
+        return input / (2.5 / 180)
+    }
+
+    /**
+     * Gets the position of the encoder.
+     *
+     * @return The position in degrees.
+     */
+    fun get(): Number {
+        return voltageToDegrees(encoder.voltage);
+    }
+}
