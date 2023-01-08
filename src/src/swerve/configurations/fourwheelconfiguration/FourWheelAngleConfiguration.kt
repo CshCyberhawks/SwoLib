cb3a8ce215c44695e0576ca52bc3ff369b61d561
cshrobotics2875 <cshrobotics@protonmail.com>
2023-01-08 19:14:01 +0000

    deploy files

--- src/src/swerve/configurations/fourwheelconfiguration/FourWheelAngleConfiguration.kt
+++
@@ -1,8 +0,0 @@
-package cshcyberhawks.swolib.swerve.configurations.fourwheelconfiguration
-
-class FourWheelAngleConfiguration(
-    var frontRight: Double = 45.0,
-    var frontLeft: Double = 135.0,
-    var backRight: Double = -45.0,
-    var backLeft: Double = -135.0
-) {}
