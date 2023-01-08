493b75728ba46910947fffc9a196e7e99dedc8ad
cshrobotics2875 <cshrobotics@protonmail.com>
2023-01-08 18:52:42 +0000

    deploy files

---
+++ src/src/math/Coordinate.kt
@@ -0,0 +1,45 @@
+package cshcyberhawks.swolib.math
+
+import java.lang.Math.toDegrees
+import java.lang.Math.toRadians
+import kotlin.math.*
+
+class Coordinate(var x: Double = 0.0, var y: Double = 0.0) {
+    var theta: Double
+        get() = toDegrees(atan2(y, x))
+        set(value) {
+            val len = r
+            x = len * cos(toRadians(value))
+            y = len * sin(toRadians(value))
+        }
+    var r: Double
+        get() = sqrt(x.pow(2) + y.pow(2))
+        set(value) {
+            val t = theta
+            x = value * cos(toRadians(t))
+            y = value * sin(toRadians(t))
+        }
+
+    companion object {
+        fun fromPolar(theta: Double, r: Double): Coordinate {
+            return Coordinate(r * sin(theta), r * cos(theta))
+        }
+    }
+
+    operator fun plus(other: Coordinate): Coordinate {
+        return Coordinate(x + other.x, y + other.y)
+    }
+
+    operator fun minus(other: Coordinate): Coordinate {
+        return Coordinate(x - other.x, y - other.y)
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Coordinate
+
+        return x == other.x && y == other.y
+    }
+}
---
+++ src/src/swerve/configurations/fourwheelconfiguration/FourWheelSpeedConfiguration.kt
@@ -0,0 +1,8 @@
+package cshcyberhawks.swolib.swerve.configurations.fourwheelconfiguration
+
+class FourWheelSpeedConfiguration(
+    var frontRight: Double = 0.5,
+    var frontLeft: Double = 0.5,
+    var backRight: Double = 0.5,
+    var backLeft: Double = 0.5
+) {}
