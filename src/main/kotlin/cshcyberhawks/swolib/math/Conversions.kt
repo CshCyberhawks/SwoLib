d09bf7e76bfc30f4ec1313d2b45f4654f4e09030
Desmond Mehta <dmehta24@csh.k12.ny.us>
2023-01-19 21:21:24 -0500

    documentation

--- src/main/kotlin/cshcyberhawks/swolib/math/Conversions.kt
+++ src/main/kotlin/cshcyberhawks/swolib/math/Conversions.kt
@@ -2,9 +2,28 @@ package cshcyberhawks.swolib.math
 
 import kotlin.math.PI
 
+/**
+ * A collection of methods for conversions
+ */
 object Conversions {
+    /**
+     * A function converting a quantity of rotations to a distance in meters with respect to gear ratio
+     * @param rotations The number of rotations to be converted
+     * @param radius The radius of the wheel
+     * @param gearRatio The net gear ratio from the motor to the wheel
+     *
+     * @return A distance in meters traveled
+     */
     fun rotationsToMeters(rotations: Double, radius: Double, gearRatio: Double): Double =
         rotations * 2 * PI * radius / gearRatio
 
+    /**
+     * A function converting a quantity of rotations to a distance in meters assuming a gear ratio of one
+     *
+     * @param rotations The number of rotations to be converted
+     * @param radius The radius of the wheel
+     *
+     * @return A distance in meters traveled
+     */
     fun rotationsToMeters(rotations: Double, radius: Double): Double = rotationsToMeters(rotations, radius, 1.0)
 }
--- src/main/kotlin/cshcyberhawks/swolib/math/Coordinate.kt
+++ src/main/kotlin/cshcyberhawks/swolib/math/Coordinate.kt
@@ -4,6 +4,14 @@ import java.lang.Math.toDegrees
 import java.lang.Math.toRadians
 import kotlin.math.*
 
+/**
+ * A class representing a coordinate point which has values and functions for polar and cartesian coordinates as well as for performing arithmetic and comparative operations with others of the same class
+ *
+ * @property x The x-coordinate of the point
+ * @property y The y-coordinate of the point
+ * @property theta The theta value of the point
+ * @property r The radius value of the point
+ */
 class Coordinate(var x: Double = 0.0, var y: Double = 0.0) {
     var theta: Double
         get() = toDegrees(atan2(y, x))
@@ -20,19 +28,58 @@ class Coordinate(var x: Double = 0.0, var y: Double = 0.0) {
             y = value * sin(toRadians(t))
         }
 
+    /**
+     * Has a function to convert polar coordinates to a coordinate class
+     */
     companion object {
+        /**
+         * Converts polar coordinates to a coordinate class
+         *
+         * @param theta The theta value of the polar coordinates
+         * @param r The radius value of the polar coordinates
+         *
+         * @return A coordinate class with cartesian coordinates
+         */
         fun fromPolar(theta: Double, r: Double): Coordinate = Coordinate(r * sin(theta), r * cos(theta))
     }
 
+    /**
+     * Adds coordinates together
+     *
+     * @param other Another coordinate which is being added
+     *
+     * @return A coordinate with coordinates being the sum of the original coordinates' coordinates
+     */
     operator fun plus(other: Coordinate): Coordinate = Coordinate(x + other.x, y + other.y)
 
+    /**
+     * Subtracts coordinates
+     *
+     * @param other Another coordinate which is being subtracted
+     *
+     * @return A coordinate with coordinates being the difference of the self coordinate and the other coordinate
+     */
     operator fun minus(other: Coordinate): Coordinate = Coordinate(x - other.x, y - other.y)
 
+    /**
+     * Multiplies the coordinates by a given value
+     *
+     * @param other The number which the coordinates are multiplied by
+     *
+     * @return It doesn't, but multiplies the coordinates of the original coordinate by the given number
+     */
     operator fun timesAssign(other: Double) {
         x *= other
         y *= other
     }
 
+    /**
+     * Compares self coordinate with another thing to test equality
+     *
+     * @param other The other value which is being compared to the coordinate
+     *
+     * @returns True if they are equal, and false if they aren't
+     */
     override fun equals(other: Any?): Boolean {
         if (this === other) return true
         if (javaClass != other?.javaClass) return false
