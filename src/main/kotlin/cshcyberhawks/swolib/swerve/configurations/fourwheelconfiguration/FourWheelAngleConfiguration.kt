1d33c4612dc0326d06ed74ca1d0a1d4922696528
a <warrenw3g@gmail.com>
2023-01-21 15:08:27 -0500

    documentation

--- src/main/kotlin/cshcyberhawks/swolib/swerve/configurations/fourwheelconfiguration/FourWheelAngleConfiguration.kt
+++ src/main/kotlin/cshcyberhawks/swolib/swerve/configurations/fourwheelconfiguration/FourWheelAngleConfiguration.kt
@@ -1,5 +1,13 @@
 package cshcyberhawks.swolib.swerve.configurations.fourwheelconfiguration
 
+/**
+ * A data class containing the angles of the wheels in a four wheel swerve configuration
+ *
+ * @property frontRight The angle of the front right swerve module
+ * @property frontLeft The angle of the front left swerve module
+ * @property backRight The angle of the back right swerve module
+ * @property backLeft The angle of the back left swerve module
+ */
 data class FourWheelAngleConfiguration(
     var frontRight: Double = -135.0,
     var frontLeft: Double = -45.0,
--- src/main/kotlin/cshcyberhawks/swolib/swerve/configurations/fourwheelconfiguration/FourWheelSpeedConfiguration.kt
+++ src/main/kotlin/cshcyberhawks/swolib/swerve/configurations/fourwheelconfiguration/FourWheelSpeedConfiguration.kt
@@ -1,5 +1,13 @@
 package cshcyberhawks.swolib.swerve.configurations.fourwheelconfiguration
 
+/**
+ * A data class containing the speed of the wheels in a four wheel swerve configuration
+ *
+ * @property frontRight The speed of the front right swerve module
+ * @property frontLeft The speed of the front left swerve module
+ * @property backRight The speed of the back right swerve module
+ * @property backLeft The speed of the back left swerve module
+ */
 data class FourWheelSpeedConfiguration(
     var frontRight: Double = 0.5,
     var frontLeft: Double = 0.5,
