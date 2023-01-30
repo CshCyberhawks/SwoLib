f7fbfece028ea133f6aa98ae07687866029141e8
Ryan <pyan4444@gmail.com>
2023-01-30 18:57:02 -0500

    Changed it to a CANCoder

--- src/main/kotlin/cshcyberhawks/swolib/hardware/implementations/SparkMaxTurnMotor.kt
+++ src/main/kotlin/cshcyberhawks/swolib/hardware/implementations/SparkMaxTurnMotor.kt
@@ -1,15 +1,16 @@
 package cshcyberhawks.swolib.hardware.implementations
 
+import com.ctre.phoenix.sensors.CANCoder
 import com.revrobotics.CANSparkMax
 import com.revrobotics.CANSparkMaxLowLevel
 import cshcyberhawks.swolib.hardware.interfaces.GenericTurnMotor
 
-class SparkMaxTurnMotor(val deviceId: Int, val offset: Double) : GenericTurnMotor {
+class SparkMaxTurnMotor(val deviceId: Int, val encoderId: Int, val offset: Double) : GenericTurnMotor {
     val motor = CANSparkMax(deviceId, CANSparkMaxLowLevel.MotorType.kBrushless)
-    val encoder = motor.encoder
+    val encoder = CANCoder(encoderId)
     override fun get(): Double = getRaw() - offset
 
-    override fun getRaw(): Double = encoder.position
+    override fun getRaw(): Double = encoder.position / 4096
 
     override fun setPercentOutput(percent: Double) {
         motor.set(percent)
