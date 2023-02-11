2957193e00c864f7a3f6858af1d33741e4fb54fc
Ryan <pyan4444@gmail.com>
2023-02-10 19:29:44 -0500

    Paths aren't going to work

--- src/main/kotlin/cshcyberhawks/swolib/autonomous/paths/AutoPath.kt
+++
@@ -1,8 +0,0 @@
-package cshcyberhawks.swolib.autonomous.paths
-
-import com.beust.klaxon.Klaxon
-import java.io.File
-
-class AutoPath(inputFile: File) {
-    val nodes: List<AutoPathNode> = Klaxon().parseArray<AutoPathNode>(inputFile)!!
-}
