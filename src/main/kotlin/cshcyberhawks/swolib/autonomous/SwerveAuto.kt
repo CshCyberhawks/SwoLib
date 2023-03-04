0aa98743bdc6c3aaeca8f5b0d78a115b7ecf8d25
Ryan <pyan4444@gmail.com>
2023-03-04 00:08:13 -0500

    Swapped the negatives

--- src/main/kotlin/cshcyberhawks/swolib/autonomous/SwerveAuto.kt
+++ src/main/kotlin/cshcyberhawks/swolib/autonomous/SwerveAuto.kt
@@ -11,6 +11,7 @@ import edu.wpi.first.math.controller.PIDController
 import edu.wpi.first.math.trajectory.TrapezoidProfile
 import edu.wpi.first.util.WPIUtilJNI
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard
+import kotlin.math.abs
 
 class SwerveAuto(
     val xPID: PIDController,
@@ -101,8 +102,12 @@ class SwerveAuto(
 
         //ryan suggested this
         val pidVal = twistPID.calculate(gyro.getYaw() / 360, desiredAngle / 360)
-        
-        val twistFF = if (pidVal > 0.1) twistFeedForward else -twistFeedForward
+
+        if (abs(pidVal) < 0.1) {
+            return -pidVal
+        }
+
+        val twistFF = if (pidVal > 0.0) twistFeedForward else -twistFeedForward
 
         if (debugLogging) {
             twistPIDOutputShuffle.setDouble(pidVal);
