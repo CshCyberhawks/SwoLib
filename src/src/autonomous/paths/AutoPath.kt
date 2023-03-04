f30f697918bf662c390df1d24564f1a3909eacd2
Milan Lustig <mguinangreen726@gmail.com>
2023-03-03 20:02:16 -0500

    rewrote the paths stuff

--- src/src/autonomous/paths/AutoPath.kt
+++ src/src/autonomous/paths/AutoPath.kt
@@ -1,31 +1,66 @@
 package cshcyberhawks.swolib.autonomous.paths
 
-import java.io.File
 import com.beust.klaxon.Klaxon
 import cshcyberhawks.swolib.autonomous.SwerveAuto
 import cshcyberhawks.swolib.autonomous.commands.GoToPosition
 import cshcyberhawks.swolib.hardware.interfaces.GenericGyro
-import cshcyberhawks.swolib.math.FieldPosition
+import cshcyberhawks.swolib.limelight.AttachedCommandType
 import cshcyberhawks.swolib.math.Vector2
 import cshcyberhawks.swolib.math.Vector3
 import edu.wpi.first.wpilibj2.command.CommandBase
+import java.io.File
 
-class AutoPath(inputFile: File, val swerveAuto: SwerveAuto, val gyro: GenericGyro) : CommandBase() {
-    val positions: List<Vector2> = Klaxon().parseArray<AutoPathNode>(inputFile)!!.map { Vector2(it.point.x, it.point.y) }
-    var currentCommand: GoToPosition? = null
+class AutoPath(
+        inputFile: File,
+        val swerveAuto: SwerveAuto,
+        val gyro: GenericGyro,
+        val commandsIn: HashMap<Int, Pair<CommandBase, AttachedCommandType>> = HashMap()
+) : CommandBase() {
+    var commandsToRun: List<CommandBase>
+
+    val positions =
+            Klaxon().parseArray<AutoPathNode>(inputFile)!!.map { Vector2(it.point.x, it.point.y) }
+
+    var currentCommand: CommandBase? = null
     var currentIndex = 1
 
     init {
+        commandsToRun =
+                Klaxon().parseArray<AutoPathNode>(inputFile)!!.map {
+                    GoToPosition(swerveAuto, Vector2(it.point.x, it.point.y))
+                }
+
         swerveAuto.swo.fieldPosition = Vector3(positions[0].x, positions[0].y, 0.0)
-//        gyro.setYawOffset(jsonData.startPosition.angle)
+        // if (commandsIn.size != 0) {
+        //     commandsIn.forEach { (key, (cmd, typ)) ->
+        //         if (key < commandsToRun.size) commandsToRun.add(cmd, key)
+        //     }
+        // }
+        //        gyro.setYawOffset(jsonData.startPosition.angle)
     }
 
     override fun execute() {
-        if ((currentCommand == null || currentCommand?.isFinished == true) && currentIndex < positions.size) {
-            currentCommand = GoToPosition(swerveAuto, positions[currentIndex++])
+        if ((currentCommand == null || currentCommand?.isFinished == true) &&
+                        currentIndex < positions.size
+        ) {
+
+            if (commandsIn.containsKey(currentIndex + 1)) {
+                val (cmd, typ) = commandsIn[currentIndex + 1]
+                when (typ) {
+                    AttachedCommandType.SYNC -> {
+                        currentCommand = cmd
+                        currentCommand?.schedule()
+                    }
+                    AttachedCommandType.ASYNC -> {
+                        cmd.schedule()
+                    }
+                }
+            }
+
+            currentCommand = commandsToRun[currentIndex++]
             currentCommand?.schedule()
         }
     }
 
-    override fun isFinished(): Boolean = currentIndex == positions.size
+    override fun isFinished(): Boolean = currentIndex == commandsToRun.size
 }
